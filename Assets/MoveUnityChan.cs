3d45d05499e450513c22a944b63f71c09aae7e68
Shea McAuley <s179049@students.aie.edu.au>
2018-08-20 17:56:55 -0700

    first commit

---
+++ Assets/MoveUnityChan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUnityChan : MonoBehaviour {
+
+    Rigidbody rb;
+    Animator anim;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>(); 
+	}
+
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        rb.velocity = input * 2.0f;
+	}
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+    public Animator anim;
+
+    public void Update()
+    {
+        bool pose1 = Input.GetKey(KeyCode.Q);
+        bool pose2 = Input.GetKey(KeyCode.W);
+        bool pose3 = Input.GetKey(KeyCode.E);
+
+        if (pose1) { anim.SetTrigger("Anim1"); }
+        if (pose2) { anim.SetTrigger("Anim2"); }
+        if (pose3) { anim.SetTrigger("Anim3"); }
+    }
+    public void Reset()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+}
